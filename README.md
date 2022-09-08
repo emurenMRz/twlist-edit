f919f25e642ca24c8ad4f78cba659282a3ad71bf
emurenMRz <emuren@mrz-net.org>
2022-09-08 11:16:51 +0900

    rename: app name

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# tw-list-edit
-
-Twitter lists can be easily edited.
+# twlist backupper
 
 Import/export of list members is supported. Lists can be moved between accounts.
 *That is the main purpose of development.
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,11 @@ import './App.scss';
 
 const BeforeLogin = () => (
   <header className="App-header">
-    <h1>TwList Editor</h1>
-    <p>Twitter lists can be easily edited.</p>
+    <h1><img src="./logo512.png" alt="logo" />TwList Backupper</h1>
+    <p>You can import and export your Twitter list.</p>
     <a
       className="App-link"
-      href="https://twlistedit.mrz-net.org/api/login"
+      href="https://twlist.mrz-net.org/api/login"
       rel="noopener noreferrer"
     >
       Login to Twitter
